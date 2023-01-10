81aaea1106570b529ef36ad1fe48c86616481d24
AlekseiBriukhachev <117446588+AlekseiBriukhachev@users.noreply.github.com>
2023-01-10 21:16:25 +0100

    Change method from for cycles to Stream API

--- src/com/aleksei/command/CaesarCipher.java
+++ src/com/aleksei/command/CaesarCipher.java
@@ -1,43 +1,51 @@
 package com.aleksei.command;
 
+import java.util.stream.IntStream;
+
 public class CaesarCipher {
     private static final String ALPHABET = "абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"
             + ".,\":-!? ";
 
     public String encryptText(String message, int key) {
-        String encryptString = "";
-        for (int i = 0; i < message.length(); i++) {
-            int letterPosition = ALPHABET.indexOf(message.charAt(i));
+        return IntStream.range(0, message.length())
+                .mapToObj(i -> encryptChar(message.charAt(i), key))
+                .collect(StringBuilder::new, StringBuilder::append, StringBuilder::append)
+                .toString();
+    }
+
+    public String decryptText(String message, int key) {
+        return IntStream.range(0, message.length())
+                .mapToObj(i -> decryptChar(message.charAt(i), key))
+                .collect(StringBuilder::new, StringBuilder::append, StringBuilder::append)
+                .toString();
+    }
+    private static char encryptChar(char c, int key) {
+        int letterPosition = ALPHABET.indexOf(c);
             char encryptChar;
             if (letterPosition >= 0) {
                 int encryptPos = (key + letterPosition) % ALPHABET.length();
                 encryptChar = ALPHABET.charAt(encryptPos);
             } else {
-                encryptChar = message.charAt(i);
+                encryptChar = c;
             }
-            encryptString += encryptChar;
-        }
-        return encryptString;
+            return encryptChar;
     }
 
-    public String decryptText(String message, int shiftKey) {
-        String decryptString = "";
-        for (int i = 0; i < message.length(); i++) {
-            int letterPosition = ALPHABET.indexOf(message.charAt(i));
+    private static char decryptChar(char c, int key) {
+        int letterPosition = ALPHABET.indexOf(c);
             char decryptChar;
             if (letterPosition >= 0) {
-                int decryptPos = (letterPosition - shiftKey) % ALPHABET.length();
+                int decryptPos = (letterPosition - key) % ALPHABET.length();
                 if (decryptPos < 0) {
                     decryptPos = ALPHABET.length() + decryptPos;
                 }
                 decryptChar = ALPHABET.charAt(decryptPos);
             }else {
-                decryptChar = message.charAt(i);
+                decryptChar = c;
             }
-            decryptString += decryptChar;
-        }
-        return decryptString;
+            return decryptChar;
     }
+
     public String getAlphabet(){
         return ALPHABET;
     }
